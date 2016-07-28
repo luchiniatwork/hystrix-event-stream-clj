7b759e23c02bafd20884891a03955fb98b75348a
Roman Gonzalez <roman@unbounce.com>
2016-07-28 11:22:39 -0700

    Add tasks to release in a simper fashion

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject hystrix-event-stream-clj "0.2.0"
+(defproject com.unbounce/hystrix-event-stream-clj "0.2.0-SNAPSHOT"
   :description "Generate hystrix event streams"
   :url "http://github.com/unbounce/hystrix-event-stream-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.stuartsierra/component "0.3.1"]
                  [com.fasterxml.jackson.core/jackson-core "2.7.5"]
@@ -12,4 +13,21 @@
                  [cheshire "5.6.3"
                   :exclusions [com.fasterxml.jackson.core/jackson-core]]]
 
-  :profiles {:dev {}})
+  :profiles {:dev {:plugins [[lein-kibit "0.1.2"]
+                             [jonase/eastwood "0.2.1"]]}}
+
+  :release-tasks [["clean"]
+                  ["kibit"]
+                  ["eastwood"]
+                  ["test"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy" "clojars"]
+                  ["change" "version"
+                   "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+
+  :deploy-repositories [["releases" :clojars]])
