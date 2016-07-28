24e3d00dfd451b56636514469f9cb188845b529d
Roman Gonzalez <roman@unbounce.com>
2016-07-28 13:37:10 -0700

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.unbounce/hystrix-event-stream-clj "0.2.0-SNAPSHOT"
+(defproject com.unbounce/hystrix-event-stream-clj "0.2.0"
   :description "Generate hystrix event streams"
   :url "http://github.com/unbounce/hystrix-event-stream-clj"
   :license {:name "Eclipse Public License"
